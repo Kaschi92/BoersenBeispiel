c741b5d807d588eb7237b6277f26dea04ad01d26
Kaschi92 <stephanie.kaschnitz@gmx.at>
2016-12-22 15:20:31 +0100

    Client-Side sogut wie fertig -> Interne Benutzer fehlen noch

--- BoersenBeispiel/BoersenBeispiel/Bank.cs
+++ BoersenBeispiel/BoersenBeispiel/Bank.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -51,6 +54,8 @@ namespace BoersenBeispiel
         public string address { get; set; }
         public string location { get; set; }
         public string telephoneNr { get; set; }
+        public Depot depot { get; set; }
+        public Account account { get; set; }
 
         // Ein Depot pro Customer
 
@@ -62,39 +67,16 @@ namespace BoersenBeispiel
             this.address = _address;
             this.location = _location;
             this.telephoneNr = _telephoneNr;
+            this.depot = createDepot();
+            this.account = new Account(this, 1000);
         }
 
-        public void createDepot()
+        private Depot createDepot()
         {
             // Kann ein Depot anlegen
+            Depot depot = new BoersenBeispiel.Depot();
+            return depot;
         }
-    }
-
-    public class Depot
-    {
-        // Beinhaltet Aktien und Kurse, Gewinn, Verlustrechnung
-        // Orderbuch - kauf/verkauf
-
-        public Depot()
-        {
-            // ToDo
-        }
-
-        public void showAktien()
-        {
-
-        }
-
-        public void showKurse()
-        {
-
-        }
-
-        public void gewinnVerlustRechnung()
-        {
-
-        }
-
     }
 
     public class Account
@@ -104,8 +86,6 @@ namespace BoersenBeispiel
         public int identifier { get; set; }
         public string accountNumber { get; set; }
         Customer customer { get; set; }
-        //GlobalVar global { get; set; }
-        
 
         public Account(Customer _cust, double _amount)
         {
@@ -121,6 +101,405 @@ namespace BoersenBeispiel
             global.setID(identifier + 1);
 
             return identifier + 1;
+        }
+
+        public void einzahlen(double betrag)
+        {
+            this.amount += betrag;
+        }
+
+        public void abheben(double betrag)
+        {
+            this.amount -= betrag;
+        }
+    }
+
+    public class Aktie
+    {
+        public string name { get; set; }
+        public Guid id { get; set; }
+        public int amount { get; set; }
+        public double course { get; set; }
+
+        public Aktie(string _name, Guid _id, int _amount, double _course)
+        {
+            this.id = _id;
+            this.name = _name;
+            this.amount = _amount;
+            this.course = _course;
+        }
+    }
+
+    public class Depot
+    {
+        private List<string> availableAktienList = new List<string>();
+        private List<string> userOrderList = new List<string>();
+        private List<Aktie> userAktienList = new List<Aktie>();
+
+        public Depot()
+        {
+
+        }
+
+        public List<string> getAvailableAktienList()
+        {
+            return availableAktienList;
+        }
+
+        public List<string> getUserOrderList()
+        {
+            return userOrderList;
+        }
+
+        public List<Aktie> getUserAktienList()
+        {
+            return userAktienList;
+        }
+
+        public void setUserAktienList(List<Aktie> aktienL)
+        {
+            this.userAktienList = aktienL;
+        }
+
+        // Option = 1 - zum Anzeigen in der Console
+        public void showAktien(string _url, int _option)
+        {
+            // REST /boerse/listCourses GET
+
+            availableAktienList.Clear();
+
+            HttpWebRequest req = (HttpWebRequest) WebRequest.Create(_url + "/boerse/listCourses");
+            req.Method = "GET";
+
+            string result = null;
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)req.GetResponse())
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    result = reader.ReadToEnd();
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        // Json parsen
+                        var objects = JArray.Parse(result);
+                        foreach (JObject root in objects)
+                        {
+                            string name = root.Value<string>("name");
+                            string aktienID = root.Value<string>("aktienID");
+                            double course = root.Value<double>("course");
+                            int amount = root.Value<int>("amount");
+
+                            StringBuilder sb = new StringBuilder();
+                            sb.Append("Name:" + name +";");
+                            sb.Append("ID:" + aktienID + ";");
+                            sb.Append("Course:" + course + ";");
+                            sb.Append("Amount:" + amount);
+
+                            if (_option == 1)
+                            {
+                                Console.WriteLine(sb.ToString());
+                            }
+                            
+                            availableAktienList.Add(sb.ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } 
+        }
+
+        public void buyAktien(string _url, string _aktienID, double _limit, int _amount, string _name)
+        {
+            // REST /boerse/buy POST
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            Guid orderID = Guid.NewGuid();
+
+            // Json erstellen
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{\n\t\"orderID\":\"" + orderID.ToString() + "\",\n");
+            sb.Append("\t\"aktienID\":\"" + _aktienID + "\",\n");
+            sb.Append("\t\"amount\":\"" + _amount + "\",\n");
+            sb.Append("\t\"limit\":\"" + _limit + "\",\n");
+            sb.Append("\t\"timestamp\":\"" + unixTimestamp + "\",\n");
+            sb.Append("\t\"hash\":\"null\"\n}");
+            string jsonString = sb.ToString();
+
+            HttpWebRequest req = (HttpWebRequest) WebRequest.Create(_url + "/boerse/buy");
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            req.ContentLength = jsonString.Length;
+
+            
+            using (StreamWriter streamWriter = new StreamWriter(req.GetRequestStream()))
+            {
+                streamWriter.Write(jsonString);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            string result = null;
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)req.GetResponse())
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    result = reader.ReadToEnd();
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        if (result == null)
+                        {
+                            Console.WriteLine("Mind. 15 Minuten warten - Check Order");
+                        }
+                        else
+                        {
+                            Console.WriteLine(result);
+                        }
+
+                        StringBuilder sb1 = new StringBuilder();
+                        sb1.Append("Name:" + _name + ";");
+                        sb1.Append("OrderID:" + orderID + ";");
+                        sb1.Append("URL:" + _url + ";");
+                        sb1.Append("AktienID:" + _aktienID + ";");
+                        sb1.Append("BUY");
+
+                        userOrderList.Add(sb1.ToString());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void sellAktien(string _url, string _aktienID, double _limit, int _amount, string _name)
+        {
+            // REST /boerse/sell POST
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            Guid orderID = Guid.NewGuid();
+
+            // Json erstellen
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{\n\t\"orderID\":\"" + orderID.ToString() + "\",\n");
+            sb.Append("\t\"aktienID\":\"" + _aktienID + "\",\n");
+            sb.Append("\t\"amount\":\"" + _amount + "\",\n");
+            sb.Append("\t\"limit\":\"" + _limit + "\",\n");
+            sb.Append("\t\"timestamp\":\"" + unixTimestamp + "\",\n");
+            sb.Append("\t\"hash\":\"null\"\n}");
+            string jsonString = sb.ToString();
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(_url + "/boerse/sell");
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            req.ContentLength = jsonString.Length;
+
+
+            using (StreamWriter streamWriter = new StreamWriter(req.GetRequestStream()))
+            {
+                streamWriter.Write(jsonString);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            string result = null;
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)req.GetResponse())
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    result = reader.ReadToEnd();
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        if (result == null)
+                        {
+                            Console.WriteLine("Mind. 15 Minuten warten - Check Order");
+                        }
+                        else
+                        {
+                            Console.WriteLine(result);
+                        }
+
+                        StringBuilder sb1 = new StringBuilder();
+                        sb1.Append("Name:" + _name + ";");
+                        sb1.Append("OrderID:" + orderID + ";");
+                        sb1.Append("URL:" + _url + ";");
+                        sb1.Append("AktienID:" + _aktienID + ";");
+                        sb1.Append("SELL");
+
+                        userOrderList.Add(sb1.ToString());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void orderCheck(string _url, string _orderID, string _name, string _aktienID, Account _acc)
+        {
+            // REST /boerse/check POST
+
+            // Json erstellen
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{\n\t\"orderID\":\"" + _orderID + "\",\n");
+            sb.Append("\t\"hash\":\"null\"\n}");
+            string jsonString = sb.ToString();
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(_url + "/boerse/check");
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            req.ContentLength = jsonString.Length;
+
+            using (StreamWriter streamWriter = new StreamWriter(req.GetRequestStream()))
+            {
+                streamWriter.Write(jsonString);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            string result = null;
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)req.GetResponse())
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    result = reader.ReadToEnd();
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        // Json parsen
+                        var root = JObject.Parse(result);
+                        string orderID = root.Value<string>("orderID");
+                        double price = root.Value<double>("price");
+                        int amount = root.Value<int>("amount");
+                        int status = root.Value<int>("status");
+                        string hash = root.Value<string>("hash");
+
+                        if (status == 0)
+                        {
+                            // Successfull
+                            Console.WriteLine("Aktion Successfull: Richtiger Amount - ist im Limit");
+
+                            foreach (string s in userOrderList)
+                            {
+                                if (s.Contains(orderID) && s.Contains("BUY"))
+                                {
+                                    _acc.abheben(price);
+                                    userAktienList.Add(new Aktie(_name, Guid.Parse(_aktienID), amount, price));
+                                }
+                                else if(s.Contains(orderID) && s.Contains("SELL"))
+                                {
+                                    // Aus Liste entfernen
+                                    _acc.einzahlen(price);
+
+                                    foreach (Aktie a in userAktienList)
+                                    {
+                                        if (a.id == Guid.Parse(_aktienID))
+                                        {
+                                            userAktienList.Remove(a);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else if (status == 1)
+                        {
+                            Console.WriteLine("Später erneut versuchen - In Progress");
+                        }
+                        else if (status == 2)
+                        {
+                            Console.WriteLine("Denied - Order wird gelöscht");
+
+                            foreach (string s in userOrderList)
+                            {
+                                if (s.Contains(orderID))
+                                {
+                                    userOrderList.Remove(s);
+                                }
+                            }
+                        }
+                        else if (status == 3)
+                        {
+                            Console.WriteLine("Nicht genügend Amount - Amount wird geändert");
+
+                            foreach (string s in userOrderList)
+                            {
+                                if (s.Contains(orderID) && s.Contains("BUY"))
+                                {
+                                    _acc.abheben(price);
+                                    userAktienList.Add(new Aktie(_name, Guid.Parse(_aktienID), amount, price));
+                                }
+                                else if (s.Contains(orderID) && s.Contains("SELL"))
+                                {
+                                    // Aus Liste entfernen
+                                    _acc.einzahlen(price);
+
+                                    foreach (Aktie a in userAktienList)
+                                    {
+                                        if (a.id == Guid.Parse(_aktienID))
+                                        {
+                                            userAktienList.Remove(a);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else if (status == 4)
+                        {
+                            Console.WriteLine("Falscher Preis - nicht im Limit - Order wird gelöscht!");
+
+                            foreach (string s in userOrderList)
+                            {
+                                if (s.Contains(orderID))
+                                {
+                                    userOrderList.Remove(s);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Unknown Error");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void gewinnVerlustRechnung(string _url, Aktie _aktie)
+        {
+            string aktienID = _aktie.id.ToString();
+
+            showAktien(_url, 0);
+            
+            foreach (string s in availableAktienList)
+            {
+                if (s.Contains(aktienID))
+                {
+                    string[] splittedString = s.Split(';');
+                    string name = splittedString[0].Split(':')[1];
+                    string id = splittedString[1].Split(':')[1];
+                    string course = splittedString[2].Split(':')[1];
+                    string amount = splittedString[3].Split(':')[1];
+
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine("Status der gekauften Aktie:");
+                    Console.WriteLine("Name: " + _aktie.name + ", Course: " + _aktie.course + ", Amount: " + _aktie.amount + ", ID: " + _aktie.id);
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine("Status der Aktie der URL:");
+                    Console.WriteLine("Name: " + name + ", Course: " + course + ", Amount: " + amount + ", ID: " + id);
+                }
+            }
         }
     }
 }
--- BoersenBeispiel/BoersenBeispiel/Program.cs
+++ BoersenBeispiel/BoersenBeispiel/Program.cs
@@ -1,20 +1,113 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace BoersenBeispiel
 {
     class Program
     {
+        static void init(Customer _externCust1, Customer _internCust1, Customer _internCust2, Customer _internCust3, string _externURL, string _internURL)
+        {
+            // Initialisiert die aktienIDs für die Verkäufer
+
+            // Für 1 Externen Customer
+            List<Aktie> externCust1List = _externCust1.depot.getUserAktienList();
+
+            _externCust1.depot.showAktien(_externURL, 0);
+            List<string> availableAktienList = _externCust1.depot.getAvailableAktienList();
+            string[] splittedString = availableAktienList[0].Split(';');
+            string name = splittedString[0].Split(':')[1];
+            string id = splittedString[1].Split(':')[1];
+            externCust1List.Add(new Aktie(name, Guid.Parse(id), 1, 180));
+            _externCust1.depot.setUserAktienList(externCust1List);
+
+            // Für die 3 Internen Customer
+            // TODO
+        }
+
         static void Main(string[] args)
         {
-            Customer cust1 = new Customer("Stephanie", "Kaschnitz", "05.07.1992", "Testaddresse 1", "Salzburg", "06641234567");
-            Account acc1 = new Account(cust1, 100);
+            string externURL1 = "https://boerse.pwnhofer.at";
+            string internURL = "http://ec2-35-165-43-90.us-west-2.compute.amazonaws.com";
 
+            // Externe USER
+            Customer cust1 = new Customer("Stephanie", "Kaschnitz", "05.07.1992", "Testaddresse 1", "Salzburg", "06641234567");
             Customer cust2 = new Customer("Christopher", "Wieland", "10.10.1993", "Testaddresse 2", "Salzburg", "06641278567");
-            Account acc2 = new Account(cust2, 100.5);
+
+            // Interne USER
+            // Verkauf
+            Customer cust3 = new Customer("Martin", "Wieser", "07.12.1991", "Testaddresse 3", "Salzburg", "06641279567");
+            Customer cust4 = new Customer("John", "Doe", "21.11.1994", "Testaddresse 4", "Salzburg", "06641273453");
+            Customer cust5 = new Customer("Max", "Mustermann", "13.07.1990", "Testaddresse 5", "Salzburg", "06641662567");
+
+            // Kauf
+            Customer cust6 = new Customer("Julia", "Musterfrau", "30.05.1992", "Testaddresse 6", "Salzburg", "06641789567");
+            Customer cust7 = new Customer("Julian", "Lipp", "22.01.1993", "Testaddresse 7", "Salzburg", "06641212347");
+            Customer cust8 = new Customer("Herbert", "Trinkdas", "01.02.1993", "Testaddresse 8", "Salzburg", "06641276666");
+
+            // Initialisieren
+            init(cust1, cust3, cust4, cust5, externURL1, internURL);
+
+            List<Aktie> aktienList1 = cust1.depot.getUserAktienList();
+
+            // Gewinn Verlust
+            Aktie aktie = aktienList1[0];
+            cust1.depot.gewinnVerlustRechnung(externURL1, aktie);
+
+            // Externer User Verkauf  
+            cust1.depot.sellAktien(externURL1, aktienList1[0].id.ToString(), 300, 1, aktienList1[0].name);
+
+            // OrderCheck
+            string orderID = null;
+            List<string> orderList = cust1.depot.getUserOrderList();
+            foreach (string s in orderList)
+            {
+                if (s.Contains(aktienList1[0].id.ToString()))
+                {
+                    string[] splittedOrderString = s.Split(';');
+                    orderID = splittedOrderString[1].Split(':')[1];
+                }
+            }
+            cust1.depot.orderCheck(externURL1, orderID, aktienList1[0].name, aktienList1[0].id.ToString(), cust1.account);
+  
+
+            // Externer user Kauf
+            cust2.depot.showAktien(externURL1, 1);
+
+            // Erste Aktie auswählen und kaufen
+            List<string> availableAktienList = cust2.depot.getAvailableAktienList();
+            string[] splittedString = availableAktienList[0].Split(';');
+            string name = splittedString[0].Split(':')[1];
+            string id = splittedString[1].Split(':')[1];
+
+            cust2.depot.buyAktien(externURL1, id, 300, 1, name);
+
+            // OrderCheck
+            orderID = null;
+            orderList = cust2.depot.getUserOrderList();
+            foreach (string s in orderList)
+            {
+                if (s.Contains(id))
+                {
+                    string[] splittedOrderString = s.Split(';');
+                    orderID = splittedOrderString[1].Split(':')[1];
+                    name = splittedOrderString[0].Split(':')[1];
+                }
+            }
+            cust2.depot.orderCheck(externURL1, orderID, name, id, cust2.account);
+
+            // 3 Interne User Verkauf
+            // TODO
+
+            // 3 Interne User Kauf
+            // TODO
+
+            Console.ReadKey();
         }
     }
 }
